4ef17743a7426310aaac834282e2ce4a36c647d2
Maschetti <mateusvmaschietto@gmail.com>
2024-06-20 10:18:30 -0300

    the functions is testable now

--- src/main.py
+++ src/main.py
@@ -2,20 +2,37 @@ from read_file import read_graph_file
 from floyd_warshall import build_matrix, floyd_warshall
 from kmeans import kmeans
 from brute_force import brute_force
+import time
+import pandas as pd
 
 if __name__ == '__main__':
-    num_nodes, num_edges, k, edges = read_graph_file('src\entradas\pmed1.txt')
-    graph = build_matrix(edges, num_nodes)
-    floyd_warshall(graph)
+    list_files = [i for i in range(1, 41)]
+    # data = {
+    #     'Time': [],
+    #     'Radius': [],
+    # }
 
-    # execute kmeans
-    clusters, radius = kmeans(graph, k)
-    for key, cluster in clusters.items():
-        print(f'Cluster {key}: {cluster}')
-    print(radius)
+    for i in list_files:
+        print(i)
+        num_nodes, num_edges, k, edges = read_graph_file(f'src\entradas\pmed{i}.txt')
+        graph = build_matrix(edges, num_nodes)
+        floyd_warshall(graph)
 
-    # execute brute_force
-    clusters, radius = brute_force(graph, k)
-    for key, cluster in clusters.items():
-        print(f'Cluster {key}: {cluster}')
-    print(radius)
+        # execute kmeans
+        start = time.time()
+        # clusters, radius = kmeans(graph, k)
+        clusters, radius = brute_force(graph, k)
+        end = time.time()
+
+        duration = end - start
+
+        df = pd.read_csv('brute_force.csv')
+
+        df.loc[len(df)] = {'Radius': radius, 'Time': duration}
+        # data['Radius'].append(radius)
+        # data['Time'].append(duration)
+
+        df.to_csv('brute_force.csv', index=False)
+
+    # df = pd.DataFrame(data)
+    # df.to_csv('brute_force.csv', index=False)
