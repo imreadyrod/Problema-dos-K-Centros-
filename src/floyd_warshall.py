eafed35ae11503068d95b0f02dec81c03191dd46
Rodrigo Oliveira <rodrigo.santos4@rhimagnesita.com>
2024-06-26 12:25:19 -0300

    alteração no método de floyd warshall

--- src/floyd_warshall.py
+++ src/floyd_warshall.py
@@ -1,3 +1,49 @@
+import os
+
+#O programa deverá acessar a pasta contendo os arquivos que possuem os dados dos grafos que serão processados
+pasta_contendo_as_entradas = "D:\Faculdade\Implementação Grafos\Problema-dos-K-Centros-\docs"
+
+#Com o caminho da pasta dos arquivos, o programa deverá ler cada arquivo sequencialmente 
+for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
+    #primeiro deverá chegar se o fim do arquivo termina com .txt
+    if nome_do_arquivo.endswith('.txt'):
+        
+        #se positivo o caminho do arquivo a ser processado será a junção da pasta com o nome do arquivo
+        caminho_do_arquivo = os.path.join(pasta_contendo_as_entradas,nome_do_arquivo)
+        nome_do_grafo = f'grafo{caminho_do_arquivo}'
+        
+        #realizar a leitura do arquivo para criar a estrutura do grafo
+        with open(caminho_do_arquivo,'r') as arquivo:
+            instrucoes = arquivo.readline().split(" ")
+            n_vertices = instrucoes[0]
+            n_arestas = instrucoes[1]
+            n_clusters = instrucoes[2]
+            grafo = [[]*n_vertices for vertice in range(n_vertices)]
+
+            for linha in arquivo:    
+                origem_destino_distancia = linha.split(" ")
+                origem = origem_destino_distancia[0]
+                destino = origem_destino_distancia[1]
+                distancia = origem_destino_distancia[2]
+                grafo[origem][destino] = distancia
+
+
+    #Após a construção do grafo, via matriz. Irei aplicar o floyd-warshall. 
+    #Assim consigo as distância de um vértice para todos os outros
+    matriz_inicial = grafo
+
+    #inicializando a matriz para realizar as iterações do método de floyd-warshall
+    matriz_das_iterações = [[-1]*n_vertices for vertice in range(n_arestas)]
+    # para aqueles vértices que não possuem ligação direta para algum dado outro vértice
+    # a distância estipulada entre eles será infinito.
+    INFINITO = 999999999999999999999
+    
+    for linha in range(n_vertices):
+        for coluna in range(n_vertices):
+            if(0 < matriz_inicial[linha][coluna] < INFINITO):
+                matriz_das_iterações[linha][coluna] = linha
+
+
 
 n = int(input())
 
@@ -56,7 +102,6 @@ for i in range(n):
 
 '''
 Entrada de exemplo
-
 5
 0 1 3 100 6
 100 0 1 3 100
