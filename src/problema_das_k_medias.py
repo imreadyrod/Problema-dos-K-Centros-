348e5dc5f723b1469167acfc16655e9f949b95d1
Rodrigo Oliveira <rodrigo.santos4@rhimagnesita.com>
2024-08-01 10:10:28 -0300

    script working and descriptive well

--- src/problema_das_k_medias.py
+++ src/problema_das_k_medias.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import time as tempo
 from itertools import combinations
 
 # para aqueles vértices que não possuem ligação direta para algum dado outro vértice
@@ -27,6 +28,7 @@ for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
         #Aqui pego o nome do arquivo contendo as respostas e a pasta onde esse arquivo está e realizo
         # a abertura desse arquivo.
         caminho_do_arquivo_resposta = os.path.join(pasta_contendo_as_respostas,nome_arquivo_resposta)
+        
 
         with open(caminho_do_arquivo_resposta,'r') as arquivo_resposta:
             #Lendo todas as linhas do arquivo resposta
@@ -48,11 +50,12 @@ for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
         # A primeira operação é a leitura da primeira linha para determinar o número de vértices,
         #  número de arestas e o número de clusters. Assim a matriz "grafo" é inicializada
 
-        
+        inicio = tempo.time()
+
         with open(caminho_do_arquivo,'r') as arquivo:
             instrucoes = arquivo.readline().split()
-            n_vertices = int(instrucoes[0])+1
-            n_arestas = int(instrucoes[1])+1
+            n_vertices = int(instrucoes[0])
+            n_arestas = int(instrucoes[1])
             n_clusters = int(instrucoes[2])
             
             # A criação da matriz deve ter n_vertices linhas e n_arestas colunas, sendo que à princípio
@@ -66,8 +69,8 @@ for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
     
             for linha in arquivo:    
                 origem_destino_distancia = linha.split()
-                origem = int(origem_destino_distancia[0])
-                destino = int(origem_destino_distancia[1])
+                origem = int(origem_destino_distancia[0])-1
+                destino = int(origem_destino_distancia[1])-1
                 distancia = int(origem_destino_distancia[2])
                 grafo[origem][destino] = distancia
                 grafo[origem][origem] = 0    
@@ -82,7 +85,7 @@ for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
     # De início a estrutura "matriz_com_caminhos_iteracao_anterior" guarda 
     # os vértices de destino para um determinado vértice de origem
     # Ela irá guardar os melhores caminhos iteração a iteração
-    matriz_com_caminhos_iteracao_anterior = [[INFINITO]*n_vertices for vertice in range(n_vertices)]
+    matriz_com_caminhos_iteracao_anterior = np.full((n_vertices,n_vertices),-1)
 
     
     # O código abaixo itera entre todos os vértice para todos os outros 
@@ -96,28 +99,55 @@ for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
                 matriz_com_caminhos_iteracao_anterior[origem][destino] = origem
 
 
+    # Floyd-Warshall é um algoritmo de programação dinâmica. A distancia atual é calculada apenas com
+    # os valores da interação anterior. Este algoritmo possui complexidade de n^3. Ou seja, para valores extremos 
+    # Fica inviável o cálculo da matriz de distancias para todos os pares de vértices. Como estamos tratando de um problema de clusterização 
+    # Faz sentido gastar um tempo para calcular de uma vez só a distância de todos para todos, pois, mais abaixo, iremos realizar a consulta dessas distâncias
+    # para cada combinação de centróide.
+    
+
     # Inicializando as matrizes necessárias para guardar os valores das iterações
     # Desta maneira, se a distância da iteração anterior for maior, o valor na matriz final deverá ser atualizado 
+    # a operação abaixo inicializa as matrizes com zeros 
+    matriz_distancias_iteracao_atual = np.zeros((n_vertices,n_vertices))
+    matriz_caminhos_iteracao_atual = np.zeros((n_vertices,n_vertices))
+    
+    
+    for intermediario in range(n_vertices):
+        for origem in range (n_vertices):
+            for destino in range(n_vertices):
 
-    matriz_distancias_iteracao_atual = [[0]*n_vertices for vertice in range(n_vertices)]
-    matriz_caminhos_iteracao_atual = [[0]*n_vertices for vertice in range(n_vertices)]
+                #A matriz Dk(i,j) é a melhor opção entre dois valores:
+                # A condição abaixo verifica se a distancia da origem até o destino é menor ou igual do que a 
+                # distância entre a origem e o destino considerando o intermediario realizando uma ponte entre esse par de vértices.
+                
+                # a soma abaixo
+                # matriz_distancia_iteracao_anterior[origem][intermediario] + matriz_distancia_iteracao_anterior[intermediario][destino]
+                # é a soma da distância entre a origem e o intermediário mais a distância entre o intermediário e o destino 
+                
+                if matriz_distancia_iteracao_anterior[origem][destino] <= matriz_distancia_iteracao_anterior[origem][intermediario] + matriz_distancia_iteracao_anterior[intermediario][destino]:
+                    
+                    # Caso a distância entre origem e destino é menor do que a distancia considerando o intermediario
+                    # A matriz final é atualizada com os valores distancia(origem,destino)
 
-    for linha in range (n_vertices):
-        for coluna in range(n_vertices):
-            for iteracao in range(n_vertices):
-                if matriz_distancia_iteracao_anterior[coluna][iteracao] <= matriz_distancia_iteracao_anterior[coluna][linha] + matriz_distancia_iteracao_anterior[linha][iteracao]:
-                    matriz_distancias_iteracao_atual[coluna][iteracao] = matriz_distancia_iteracao_anterior[coluna][iteracao]
-                    matriz_caminhos_iteracao_atual[coluna][iteracao] = matriz_caminhos_iteracao_atual[coluna][iteracao]
+                    matriz_distancias_iteracao_atual[origem][destino] = matriz_distancia_iteracao_anterior[origem][destino]
+                    matriz_caminhos_iteracao_atual[origem][destino] = matriz_com_caminhos_iteracao_anterior[origem][destino]
                 else:
-                    matriz_distancias_iteracao_atual[coluna][iteracao] = matriz_distancias_iteracao_atual[coluna][iteracao] + matriz_distancias_iteracao_atual[iteracao][coluna]
-                    matriz_caminhos_iteracao_atual[coluna][iteracao] = matriz_com_caminhos_iteracao_anterior[linha][iteracao]
+
+                    # Caso o custo minimo da origem ate a distancia considerando o vértice intermediario seja menor
+                    # Então a matriz das distancias e dos caminhos é atualizada com 
+                    # A distancia sendo a soma entre matriz_distancia_iteracao_anterior[origem][intermediario] + matriz_distancia_iteracao_anterior[intermediario][destino]
+                    # E o caminho é origem- intermediario - destino
+
+                    matriz_distancias_iteracao_atual[origem][destino] = matriz_distancia_iteracao_anterior[origem][intermediario] + matriz_distancia_iteracao_anterior[intermediario][destino]
+                    matriz_caminhos_iteracao_atual[origem][destino] = matriz_com_caminhos_iteracao_anterior[intermediario][destino]
                 ## Após realizar o cálculos dos novos pesos e também determinar os melhores caminhos para a iteração atual
                 # As matrizes devem ser atualizadas
-                for linha in range(n_vertices):
-                    for coluna in range(n_vertices):
-                        matriz_distancia_iteracao_anterior[linha][coluna] = matriz_distancias_iteracao_atual[linha][coluna]
-                        matriz_com_caminhos_iteracao_anterior[linha][coluna] = matriz_caminhos_iteracao_atual[linha][coluna]
-        
+        for origem in range(n_vertices):
+            for destino in range(n_vertices):
+                matriz_distancia_iteracao_anterior[origem][destino] = matriz_distancias_iteracao_atual[origem][destino]
+                matriz_com_caminhos_iteracao_anterior[origem][destino] = matriz_caminhos_iteracao_atual[origem][destino]
+
     
     # Para saber a distancia do vértice i até o vértice j, ou seja, o seu custo, deve-se utilizar a matriz
     # matriz_caminhos_iteracao_atual. Esta matriz ao final do programa irá conter a distâncias de todos os vértices 
@@ -127,10 +157,7 @@ for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
     # A proxima etapa do código consiste em realizar todas as combinações possíveis de n cluster em t numero de vértices.
     # Desta maneira, para um conjunto de 100 vértices, todas as combinações possíveis para 5 clusters resulta em 75.287.520 
     # de conjuntos possíveis
-    def encontre_mais_proximo(elemento,lista):
-        return min(lista, key=lambda x:abs(x-elemento))
     
-
     numeros = list(range(n_vertices))
     melhor_combinacao = [0,0,0,0]
     raio_resposta = 0
@@ -141,29 +168,35 @@ for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
         # Então:
         #transformando a tupla em uma lista
         centroides_selecionados = [elemento for elemento in combinacao]
-        # Criando uma lista para conter os elemento do cluster
-        clusters = [elemento for elemento in centroides_selecionados]
         
         raio_clusters = [0 for elemento in centroides_selecionados]
         # Adicionando o valor 0 para inicializar o raio do cluster 
         # Iterando sobre os vértices 
-        for vertice in n_vertices:
+        for vertice in range(n_vertices):
             # Por exemplo, para o vértice 1, devo saber qual a menor distância entre os centróides selecionados
             # Assim tenho que verificar cada distância em relação ao centróide e pegar a mínima
             distancia_minima = INFINITO
 
+            # Entrando na lista dos centroides:
             for centroide in centroides_selecionados:
                 # Aqui busco a distância do vértice atual para o centróide selecionado
-                distancia_temporaria = matriz_caminhos_iteracao_atual[centroide][vertice]
+                distancia_temporaria = matriz_distancias_iteracao_atual[centroide][vertice]
                 # Se a distancia da iteração for menor do que a distancia minima 
-                # A distancia minima é atualizada em conjunto com o centroide
+                # A distancia minima é atualizada. O vértice tem que pertencer ao centro de menor distância!
                 if distancia_temporaria <= distancia_minima:
                     distancia_minima = distancia_temporaria
                     centroide_do_vertice = centroide
-            # Após a iteração irei saber em qual cluster esse vértice pertence
-            posicao_do_centroide_na_lista = clusters.index(centroide)
+                #A iteração se repete para todos os centros. 
+                # Aquele centro que possui a menor distância em relação ao vértice é cluster desse vértice.
+                
+            # Após a iteração entre todos os centroides irei saber em qual cluster esse vértice pertence
+            #Abaixo eu armazeno a posição do centróide selecionado para aquele vértice
+            posicao_do_centroide_na_lista = centroides_selecionados.index(centroide_do_vertice)
             # Após encontrar a posição do centróide
-            # Devo adicionar a distancia máxima, ou seja, o raio para aquele cluster na lista dos raios
+            # Devo adicionar a distancia máxima, ou seja, o raio para daquele cluster na lista dos raios
+            # Então sempre verifico se para o vértice em questão, a sua distância em relação ao centro é maior do que 
+            # a distância de um outro vértice que ja foi processado, pois, ao final, quero apenas a distância do vértice mais afastado
+            # do centro. 
             raio_temporario = distancia_minima
             if raio_clusters[posicao_do_centroide_na_lista] <= raio_temporario:
                 raio_clusters[posicao_do_centroide_na_lista] = raio_temporario
@@ -176,22 +209,23 @@ for nome_do_arquivo in os.listdir(pasta_contendo_as_entradas):
         #Devo guardar a combinação para no final conferir qual foi a combinação que gerou o resultado mais próximo do
         #esperado
         #Aqui estou comparando a resposta do raio anterior com o raio atual
-        alvo = raio_solucao
-        combinacao_temporaria = melhor_combinacao
-        raio_anterior = raio_resposta
-        raio_atual = maior_raio
-        if abs(alvo - raio_anterior) < abs(alvo - raio_atual):
-            combinacao_resposta = combinacao_temporaria
-            raio_resposta = raio_anterior
-        else:
-            combinacao_resposta = combinacao
-            raio_resposta = raio_atual
+        if abs(raio_solucao - raio_resposta) > abs(raio_solucao - maior_raio):
+            combinacao_resposta = centroides_selecionados
+            raio_resposta = maior_raio
         
-        print(f'Arquivo: {nome_do_grafo}')
-        print(f'melhor combinacao: {combinacao_resposta}')
-        print(f'raio da combinacao: {raio_resposta}')
+        #print(f'Arquivo: {nome_do_grafo}')
+        #print(f'melhor combinacao: {combinacao_resposta}')
+        #print(f'raio da combinacao: {raio_resposta}')
 
-        
+    fim = tempo.time()
+    tempo_processamento = fim-inicio
+    
+    arquivo_resultado = os.path.join(pasta_contendo_as_respostas,nome_do_arquivo)
+    texto_resposta = f"resposta do arquivo: {nome_do_arquivo}\nmelhor combinacao: {combinacao_resposta}\nraio da combinacao: {raio_resposta}\ntempo de processamento{tempo_processamento}\n"
+    with open(arquivo_resultado,'a') as arquivo_resposta:
+        arquivo_resposta.writelines(texto_resposta)
+    
+    
 
 
 
